f6483048b4a2e553075e1f075192c72aab0ebb90
cometsong <benjamin@cometsong.net>
2019-10-11 10:34:26 -0400

    remove __all__ from tprobe/__init__.py

--- tprobe/__init__.py
+++ tprobe/__init__.py
@@ -2,17 +2,3 @@ from .db import SqliteIO
 from .abspath import AbsPath
 from .config import CONFIG, DB_CFG, read_config_file, write_config_file
 from .log import log, log_init
-
-__all__ = [
-    log,
-    log_init,
-    AbsPath,
-    SqliteIO,
-    CONFIG,
-    DB_CFG,
-    read_config_file,
-    write_config_file,
-]
-
-__author__ = 'Benjamin Leopold'
-
